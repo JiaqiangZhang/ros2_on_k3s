4999a727dfeb67ef7dc72d6b7de5dd7ed37439b9
Jiaqiang Zhang <jizhan@utu.fi>
2023-03-02 16:05:05 +0200

    modify dockerfile and shell

--- entrypoint_ros2_dockerfile.sh
+++ entrypoint_ros2_dockerfile.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -e
+# arg 
+ROS_DISTRO=$1 
+ROS_DOMAIN_ID=$2
+IMAGE_TOPIC=$3
+YOLO_MODEL=$4
 
 # set ros environment
-source /opt/ros/${ROS_DISTRO}/setup.bash 
-source ./install/setup.bash 
+source /opt/ros/${ROS_DISTRO}/setup.bash
+source ./install/setup.bash
 export ROS_DOMAIN_ID=${ROS_DOMAIN_ID}
 
 ros2 run yolov5_ros2 yolo_detect --ros-args -p image_topic:=/${IMAGE_TOPIC} -p model:=${YOLO_MODEL}
