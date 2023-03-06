b3da9110ae4350593e4dce48a46329cfe84a2800
JiaqiangZhang <jizhan@utu.fi>
2023-03-06 18:08:22 +0200

    Add arg modify entrypoint_ros2_dockerfile.sh and entrypoint_ros2_dockerfile to enable arg when run docker container

--- entrypoint_ros2_dockerfile.sh
+++ entrypoint_ros2_dockerfile.sh
@@ -1,10 +1,48 @@
 #!/bin/bash
 set -e
 # arg 
-ROS_DISTRO=$1 
-ROS_DOMAIN_ID=$2
-IMAGE_TOPIC=$3
-YOLO_MODEL=$4
+ROS_DISTRO="galactic" #$1 
+ROS_DOMAIN_ID=7 #$2
+IMAGE_TOPIC="image_raw" #$3
+YOLO_MODEL="yolov5s" #$4
+
+usage()
+{
+    echo "Usage: entrypoint yolov5 ros2 [ --ROS_DISTRO foxy or galactic ] 
+                                [ --ROS_DOMAIN_ID default 7 ] 
+                                [ --IMAGE_TOPIC default image_raw] 
+                                [ --YOLO_MODEL default yolov5s ]"
+    echo $@
+}
+
+# PARSED_ARGUMENTS=$(getopt -a -n entrypoint_ros2_dockerfile --long ROS_DISTRO:,ROS_DOMAIN_ID:,IMAGE_TOPIC:,YOLO_MODEL: -- "$@")
+PARSED_ARGUMENTS=$(getopt -n entrypoint_ros2_dockerfile -o R:I:T:M: -- "$@")
+
+VALID_ARGUMENTS=$?
+if [ "$VALID_ARGUMENTS" != "0" ]; then
+    usage
+fi
+
+echo "PARSED_ARGUMENTS IS $PARSED_ARGUMENTS"
+eval set -- "$PARSED_ARGUMENTS"
+
+while :
+do
+    case "$1" in
+        # --ROS_DISTRO) ROS_DISTRO="$2" ; shift 2 ;;
+        # --ROS_DOMAIN_ID) ROS_DOMAIN_ID="$2" ; shift 2 ;;
+        # --IMAGE_TOPIC) IMAGE_TOPIC="$2" ; shift 2 ;;
+        # --YOLO_MODEL) YOLO_MODEL="$2" ; shift 2 ;;
+
+        -R) ROS_DISTRO="$2" ; shift 2 ;;
+        -I) ROS_DOMAIN_ID="$2" ; shift 2 ;;
+        -T) IMAGE_TOPIC="$2" ; shift 2 ;;
+        -M) YOLO_MODEL="$2" ; shift 2 ;;
+        --) shift; break ;;
+        *) echo "Unexpected option"
+            usage ;;
+    esac
+done
 
 # set ros environment
 source /opt/ros/${ROS_DISTRO}/setup.bash
@@ -12,3 +50,5 @@ source ./install/setup.bash
 export ROS_DOMAIN_ID=${ROS_DOMAIN_ID}
 
 ros2 run yolov5_ros2 yolo_detect --ros-args -p image_topic:=/${IMAGE_TOPIC} -p model:=${YOLO_MODEL}
+
+exec "$@"
